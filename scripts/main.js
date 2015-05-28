263e6032c7e130829f48be462d720e6ffc0ab639
Losses Don <losses.don@gmail.com>
2015-05-28 20:30:04 +0800

    支付页面

--- scripts/main.js
+++ scripts/main.js
@@ -2,32 +2,88 @@
  * Created by Don on 1/31/2015.
  */
 
-(function () {
-    var headerLis = document.querySelectorAll('.nav_item') /*nxt line*/
-        , highlightItem = document.querySelector('.highlight');
-    for (var x = 0; x < headerLis.length; x++) {
-        $(headerLis[x]).on({
-                'mouseenter': function (event) {
-                    $(highlightItem).css('right', $("body").width() - ($(event.target).offset().left + 75));
-                    $(this).addClass('high');
+var a;
 
-                    event.stopPropagation();
-                },
-                'mouseleave': function () {
-                    $(this).removeClass('high');
-                }
+angular.module('rM', ['ngRoute', 'ngAnimate']).
+
+    config(['$controllerProvider', '$routeProvider',
+        function ($controllerProvider, $routeProvider) {
+            $routeProvider.
+                when('/', {
+                    templateUrl: 'partials/index.html',
+                    controller: 'postCtrl'
+                }).
+                when('/panel', {
+                    templateUrl: 'partials/panel.html',
+                    controller: 'panelCtrl'
+                }).
+                otherwise({redirectTo: '/'})
+        }]).
+
+    controller('postCtrl', function () {
+        console.log('!');
+    }).
+    controller('panelCtrl', function ($scope) {
+        //
+    }).
+    controller('headerCtrl', function ($scope, $location) {
+
+        $scope.getClass = function () {
+            var headerClass;
+            switch ($location.path()) {
+                case '/':
+                    headerClass = 'index';
+                    break;
+                case '/panel':
+                    headerClass = 'panel';
+                    break;
             }
-        );
-    }
+            return headerClass;
+        };
+    }).
 
-    $('nav').on('mouseleave', function (event) {
-        highlightItem.setAttribute('style', 'right:-200px;');
+    directive('paymentIcon', function () {
+        return {
+            restrict: 'E',
+            require: '?ngModel',
+            template: '<label>' +
+            '<input class="payment-input" type="radio" name="payment" ng-model="model" value="{{value}}">' +
+            '<i class="payment_wrapper" style="background:url(\'{{payIcon}}\') no-repeat center;"></i>' +
+            '</label>',
+            scope: {value: '@value', payIcon: '@payIcon', model: '='},
+            replace: false
+        };
 
-        event.stopPropagation();
-        event.preventDefault();
     });
 
-    $('#close').on('click',function(){
+(function () {
+    var body = $(document.body);
+    var highlight = $(".highlight");
+    var nav = $("header > nav");
+    nav.find("li").mouseenter((function () {
+        var timeoutId;
+
+        return function () {
+            var $this = $(this);
+
+            if (timeoutId)
+                clearTimeout(timeoutId);
+
+            highlight.css("left", $this.position().left).width($this.width());
+
+            if (body.hasClass("touch_screen")) {
+                timeoutId = setTimeout(function () {
+                    highlight.css("left", 1000);
+                }, 500);
+            }
+        }
+    })());
+    nav.mouseleave(function () {
+        if (!body.hasClass("touch_screen"))
+            highlight.css("left", 1000);
+    });
+
+    $('#close').on('click', function () {
         $('#highlight').slideUp();
-    })
+    });
 })();
