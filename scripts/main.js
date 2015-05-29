17f8b7e3d250e6205ee6b942a95934fab1ba297c
Losses Don <losses.don@gmail.com>
2015-05-29 11:05:08 +0800

    自定义页面合并

--- scripts/main.js
+++ scripts/main.js
@@ -17,29 +17,110 @@ angular.module('rM', ['ngRoute', 'ngAnimate']).
                     templateUrl: 'partials/panel.html',
                     controller: 'panelCtrl'
                 }).
+                when('/panel/:ndAction', {
+                    templateUrl: 'partials/panel.html',
+                    controller: 'panelCtrl'
+                }).
+                when('/panel/:ndAction/:rdAction', {
+                    templateUrl: 'partials/panel.html',
+                    controller: 'panelCtrl'
+                }).
+                when('/panel/:ndAction/:rdAction/:thAction', {
+                    templateUrl: 'partials/panel.html',
+                    controller: 'panelCtrl'
+                }).
+                when('/panel/:ndAction/:rdAction/:thAction/:fiAction', {
+                    templateUrl: 'partials/panel.html',
+                    controller: 'panelCtrl'
+                }).
+                when('/:customCommand', {
+                    templateUrl: 'partials/page.html',
+                    controller: 'pageCtrl'
+                }).
                 otherwise({redirectTo: '/'})
         }]).
 
     controller('postCtrl', function () {
         console.log('!');
     }).
-    controller('panelCtrl', function ($scope) {
-        //
+    controller('panelCtrl', function ($scope, $location, $http) {
+        $scope.parseLocation = function () {
+            return $location.path().split('/').splice(1)
+        };
+
+        $scope.checkLocation = function (locationTarget, position) {
+            return $scope.parseLocation()[position] === locationTarget;
+        };
+
+        $scope.removeMd = function (text) {
+            return text.split('.md')[0];
+        };
+
+        $scope.location = $scope.parseLocation();
+
+        if ($scope.location[1] === 'help') {
+            $scope.helpContent = 'loading';
+
+            if ($scope.location[3] === undefined)
+                $scope.helpContent = error;
+
+            $http.get('dbs/guide/' + $scope.location[2] + '/' + $scope.location[3] + '.md').
+                success(function (data) {
+                    $scope.helpContent = markdown.toHTML(data);
+                }).
+                error(function () {
+                    $scope.helpContent = 'not found!'
+                });
+
+            $http.get('dbs/guide/context.json').
+                success(function (data) {
+                    $scope.helpContext = data;
+                }).
+                error(function () {
+                    console.log('error,cant get the context file!')
+                })
+        }
     }).
     controller('headerCtrl', function ($scope, $location) {
-
         $scope.getClass = function () {
+            var currentPath = $location.path();
+
             var headerClass;
-            switch ($location.path()) {
-                case '/':
-                    headerClass = 'index';
-                    break;
-                case '/panel':
-                    headerClass = 'panel';
-                    break;
-            }
+            if (currentPath === '/')
+                headerClass = 'index';
+            else if (currentPath.match(/^\/panel/) !== null)
+                headerClass = 'panel';
+            else
+                headerClass = 'page';
+
             return headerClass;
         };
+    }).
+    controller('pageCtrl', function ($location, $scope, $http) {
+        var location = $location.path().split('/')[1];
+
+        $scope.pageContent = 'loading...';
+
+        $http.get('dbs/page/context.json').
+            success(function (data) {
+                if (data[location]) {
+                    var fileName = data[location].fileName;
+                    $http.get('dbs/page/' + fileName).
+                        success(function (fileContent) {
+                            var parseType = data[location]['parse'] ? data[location]['parse'] : 'markdown';
+                            $scope.pageContent = (parseType === 'markdown') ? markdown.toHTML(fileContent) : fileContent;
+                        }).
+                        error(function () {
+                            $scope.pageContent = 'error, cant get the page file.'
+                        })
+                } else {
+                    $scope.pageContent = 'error, page not found'
+                }
+            }
+        ).
+            error(function () {
+                $scope.pageContent = 'error, cant get the context.'
+            });
     }).
 
     directive('paymentIcon', function () {
@@ -54,6 +135,12 @@ angular.module('rM', ['ngRoute', 'ngAnimate']).
             replace: false
         };
 
+    }).
+
+    filter('trustHtml', function ($sce) {
+        return function (input) {
+            return $sce.trustAsHtml(input);
+        }
     });
 
 (function () {
